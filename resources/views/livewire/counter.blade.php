d5176b5f2d9b029fac64de316389abe782c834ab
Yousef Azzam <yzam782@gmail.com>
2025-04-03 14:34:05 +0300

    add changes on counter file

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,3 +1,7 @@
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    <h1>{{ $count }}</h1>
+ 
+    <button wire:click="increment">+</button>
+ 
+    <button wire:click="decrement">-</button>
 </div>
