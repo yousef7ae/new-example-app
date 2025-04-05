d2881b252d828523fd6c3fc45bd9b419f32fc721
Yousef Azzam <yzam782@gmail.com>
2025-04-05 15:30:47 +0300

    change on index file

--- app/Livewire/index.php
+++ app/Livewire/index.php
@@ -6,8 +6,14 @@ use Livewire\Component;
 
 class Index extends Component
 {
+    public $count = 1;
     public function render()
     {
         return view('livewire.index');
+    }
+
+    public function increment()
+    {
+        this->count++;
     }
 }
